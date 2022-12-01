c2eb74f05c599d95e993915f29082d0c2a7b00ed
enzo <Eynzof@gmail.com>
2022-12-01 11:22:55 +0800

    import pdf as module

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './about.module.css';
 import AboutImg from "../../../assets/about.jpg";
+import CV from "../../../assets/John-Cv.pdf"
 
 const About = () => {
     return (
@@ -9,7 +10,10 @@ const About = () => {
                 <h2 className="section__title">About Me</h2>
                 <span className="section__subtitle">My Introduction</span>
 
-                <div className="about__container container grid"></div>
+                <div className="about__container container grid">
+                    <img src={AboutImg.src} alt="" className="about__img"/>
+                    <div className="about__data"></div>
+                </div>
             </section>
         </div>
     );
