64c15e22cacf3df71e155fd3e84e1fad1834826b
enzo <Eynzof@gmail.com>
2022-12-02 10:11:28 +0800

    add skills

--- src/components/skills/Frontend.tsx
+++ src/components/skills/Frontend.tsx
@@ -4,10 +4,57 @@ import styles from './skill.module.css'
 const Frontend = () => {
     return (
         <div className={styles.skills__content}>
-            <h3 className={styles.skills__title}></h3>
+            <h3 className={styles.skills__title}>前端</h3>
             <div className={styles.skills__box}>
                 <div className={styles.skills__group}>
-                    <div className={styles.skills__data}></div>
+                    <div className={styles.skills__data}>
+                        <i className={`bx bx-badge-check ${styles.skills__icon}`}></i>
+                        <div>
+                            <h3 className={styles.skills__name}>HTML</h3>
+                            <span className={styles.skills_level}>初级</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.skills__data}>
+                        <i className={`bx bx-badge-check ${styles.skills__icon}`}></i>
+                        <div>
+                            <h3 className={styles.skills__name}>CSS</h3>
+                            <span className={styles.skills_level}>中级</span>
+                        </div>
+                    </div>
+                    <div className={styles.skills__data}>
+                        <i className={`bx bx-badge-check ${styles.skills__icon}`}></i>
+                        <div>
+                            <h3 className={styles.skills__name}>JavaScript</h3>
+                            <span className={styles.skills_level}>中级</span>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={styles.skills__group}>
+                    <div className={styles.skills__data}>
+                        <i className={`bx bx-badge-check ${styles.skills__icon}`}></i>
+                        <div>
+                            <h3 className={styles.skills__name}>Git</h3>
+                            <span className={styles.skills_level}>中级</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.skills__data}>
+                        <i className={`bx bx-badge-check ${styles.skills__icon}`}></i>
+                        <div>
+                            <h3 className={styles.skills__name}>Bootstrap</h3>
+                            <span className={styles.skills_level}>中级</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.skills__data}>
+                        <i className={`bx bx-badge-check ${styles.skills__icon}`}></i>
+                        <div>
+                            <h3 className={styles.skills__name}>React</h3>
+                            <span className={styles.skills__level}>中级</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -8,7 +8,10 @@ const Skills = () => {
         <section className="skills section" id="skills">
             <h2 className="section__title">技能</h2>
             <span className="section__subtitle">我的技术栈</span>
-            <div className={`${styles.skills__container} container grid`}></div>
+            <div className={`${styles.skills__container} container grid`}>
+                <Frontend/>
+                <Backend/>
+            </div>
         </section>
     );
 };
