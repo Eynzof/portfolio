38fbe37bec8559696acd47e56b9a81cec3812b41
enzo <Eynzof@gmail.com>
2022-12-03 19:58:43 +0800

    scrollup section

--- src/components/scrollup/ScrollUp.tsx
+++ src/components/scrollup/ScrollUp.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import styles from "./scrollup.module.css";
 
 const ScrollUp = () => {
-  return <div></div>;
+  const onScroll = useCallback(() => {
+    const { scrollY } = window;
+    if (scrollY >= 560) {
+      const scrollUp = document.getElementById("scroll-up");
+      // @ts-ignore
+      scrollUp.classList.add("show__scroll");
+    } else {
+      const scrollUp = document.getElementById("scroll-up");
+      // @ts-ignore
+      scrollUp.classList.remove("show__scroll");
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll, { passive: true });
+
+    return () => {
+      // @ts-ignore
+      window.removeEventListener("scroll", onScroll, { passive: true });
+    };
+  }, []);
+
+  return (
+    <a href="#" className={`${styles.scrollup}`} id="scroll-up">
+      <i className={`${styles.scrollup__icon} uil uil-arrow-up`}></i>
+    </a>
+  );
 };
 
 export default ScrollUp;
