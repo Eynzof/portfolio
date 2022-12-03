d6eb77b362f8e8861c924ab59af0fb0f7be7ca1d
enzo <Eynzof@gmail.com>
2022-12-03 19:23:21 +0800

    adjust page structure

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -32,6 +33,23 @@ const Footer = () => {
         <span className={`${styles.footer__copy}`}>
           &#169; Enzo Li. All rights reserved
         </span>
+        <div className={`${styles.footer__vercel}`}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.footer__link}`}
+          >
+            Powered by
+            <Image
+              className={`${styles.footer__vercel__logo}`}
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
